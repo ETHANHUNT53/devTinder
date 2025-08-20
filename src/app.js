8d157c636088d7c15396dec9e7eb5482cea2967c
Shaurya Jaiswal <shreyjaiswal411@gmail.com>
2025-08-20 23:17:15 +0530

    middleware

--- src/app.js
+++ src/app.js
@@ -2,18 +2,22 @@ const express = require("express");
 
 const app = express();
 
+const { adminAuth, userAuth } = require("./middlewares/auth");
+
 app.listen(3000, () => {
   console.log("Server is running on PORT 3000");
 });
 
-app.use("/", (req, res) => {
-  res.send("Hello World!");
+app.use("/admin", adminAuth);
+
+app.get("/user", userAuth, (req, res) => {
+  res.send("All data sent");
 });
 
-app.use("/hello", (req, res) => {
-  res.send("Hello Hello!");
+app.get("/admin/getAllData", (req, res) => {
+  res.send("All data sent");
 });
 
-app.use("/test", (req, res) => {
-  res.send("Hello Test!");
+app.get("/admin/deleteUser", (req, res) => {
+  res.send("deleted user");
 });
